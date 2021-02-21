a74b296e1240b9a9e1d3e057809248a83fa2f045
Evandro Picanço Júnior <expectpin@users.noreply.github.com>
2021-02-21 15:21:05 -0400

    Add Aromas enum

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,37 @@ const Types = Object.freeze({
   AMBIENTE: Symbol('Ambiente')
 })
 
+const Aromas = Object.freeze({
+  LAVANDA: Symbol('Lavanda'),
+  ROSAS_VERMELHAS: Symbol('Rosas Vermelhas'),
+  BREU_BRANCO: Symbol('Breu Branco'),
+  FLOR_DE_AMEIXA: Symbol('Flor de Ameixa'),
+  FLOR_DE_LARANJEIRA: Symbol('Flor de Laranjeira'),
+  FLOR_DE_CEREJEIRA: Symbol('Flor de Cerejeira'),
+  FLORAL_BEBE: Symbol('Floral Beb&#234;'),
+  CHEIRINHO_DE_BEBE: Symbol('Cheirinho de Beb&#234;'),
+  SANDALO: Symbol('S&#226;ndalo'),
+  CAPIM_LIMAO: Symbol('Capim Lim&#227;o'),
+  ALECRIM: Symbol('Alecrim'),
+  BAUNILHA: Symbol('Baunilha'),
+  ACAI: Symbol('A&#231;a&#237;'),
+  FLOR_TUPINAMBA: Symbol('Flor Tupinamb&#225;'),
+  MULATEIRO: Symbol('Mulateiro'),
+  MURU_MURU: Symbol('Muru Muru'),
+  PATMIRRA: Symbol('Patmirra'),
+  AMBALY: Symbol('Ambaly'),
+  ALGODAO: Symbol('Algod&#227;o'),
+  BAMBU: Symbol('Bambu'),
+  MAMAE_BEBE: Symbol('Mam&#227;e Beb&#234;'),
+  BURUTI: Symbol('Buriti')
+})
+
 class Item {
-  constructor (name, price, type, aroma) {
+  constructor (name, price, type, {aromas}) {
     this.name = name
     this.price = price
     this.type = type
-    this.aroma = aroma
+    this.aroma = new Array(aromas)
   }
 }
 
